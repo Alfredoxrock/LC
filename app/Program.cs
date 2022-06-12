4ac0d2ccdda070f3eedf020ac28b8eacb38346c3
Alfredoxrock <alf_baza@hotmail.com>
2022-06-11 21:26:30 -0600

    New problem solved

--- app/Program.cs
+++ app/Program.cs
@@ -22,6 +22,29 @@ namespace app
         }
         return false;
        }
+
+       public ListNode DetectCycle(ListNode head) {
+        if(head == null || head.next == null) return null;
+        ListNode slow = head;
+        ListNode fast = head;
+        while( fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast) break;
+        }
+        
+        if(fast == null || fast.next == null) return null;
+        
+        fast = head;
+        
+        while (fast != slow){
+            fast = fast.next;
+            slow = slow.next;
+        }
+        
+        return fast;
+        
+    }
         
     }
 }
