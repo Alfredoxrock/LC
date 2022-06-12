01972fc2cffdf2ddc996108320fef27234dc64b7
Alfredoxrock <alf_baza@hotmail.com>
2022-06-11 18:05:02 -0600

    New classes

---
+++ app/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+       public static bool HasCycle(ListNode head){
+        if (head == null) return false;
+        // Create a temporary node
+        var slow = head;
+        var fast = head.next;
+        while (fast != null && fast.next != null){
+            if (slow == fast) return true;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return false;
+       }
+        
+    }
+}
