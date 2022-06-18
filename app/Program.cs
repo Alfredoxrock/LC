269cea2c15be095e8c0242cd4c7f15cb8ff46416
Alfredoxrock <alf_baza@hotmail.com>
2022-06-17 22:35:19 -0600

    New Algorithm

--- app/Program.cs
+++ app/Program.cs
@@ -138,6 +138,25 @@ namespace app
         }
         return previous;
     }
+
+    public ListNode removeElements(ListNode head, int val) {
+         while(head != null && head.val == val){
+            head = head.next;
+        }
+        
+        ListNode current = head;
+        while(current != null && current.next != null){
+            if(current.next.val == val){
+                current.next = current.next.next;
+            }
+            else
+            {
+                current = current.next;
+            }
+        }
+        
+        return head;
+    }
         
     }
 }
